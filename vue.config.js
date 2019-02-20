edeab670bbf08e6aaeea069edab74459ff7e0170
pabloa164 <pabloa164@gmail.com>
2019-02-20 10:19:00 -0500

    update vue.conf.js file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/lyrics-search-app-vue/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/pomodoro-timmer/' : '/'
 }
